1b83e18c8537669d48c219b717a201e15504509f
codebudy <a.shekhar5247@gmail.com>
2023-02-08 18:38:32 +0530

    update

---
+++ controllers/apiController.js
@@ -0,0 +1,178 @@
+require('dotenv').config()
+
+const { EasyModal } = require('../models/EasyStarter')
+const {TokenLock} = require('../models/TokenLock')
+const {DevidendToken} = require('../models/DevidendTokenInfo')
+const {SimpleToken} = require('../models/SimpleTokenInfo')
+const {StandardToken} = require('../models/StandardTokenInfo')
+const {ReflectionToken} = require('../models/ReflectionTokenInfo')
+const config = require('../config/config')
+const fs = require('fs');
+
+getPoolsList = async () => {
+	try {
+		var rows = await EasyModal.find({ state: 10 }).sort({
+			startTime: 1,
+		})
+		return { err: 0, result: rows }
+	} catch (ex) {
+		console.log(ex)
+		return { err: 1, msg: "poolList: cannot found pool data" }
+	}
+}
+
+getMyPools = async (ownerAddress) => {
+	try {
+		var row = await EasyModal.find({ ownerAddress: ownerAddress }).sort({
+			startTime: 1
+		})
+		return { err: 0, result: row }
+	} catch (ex) {
+		console.log(ex)
+		return { err: 1, msg: "poolData: cannot found pool data" }
+	}
+}
+
+getPoolInfo = async (id) => {
+	try {
+		var row = await EasyModal.findOne({ _id: id })
+		return { err: 0, result: row }
+	} catch (ex) {
+		console.log(ex)
+		return { err: 1, msg: "poolData: cannot found pool data" }
+	}
+}
+
+createSimpleToken = async (props) => {
+	try {
+		var rows = await SimpleToken.find({ tokenAddress: props?.tokenAddress, state: 10 })
+		if (rows.length > 0) return { error: 2, msg: 'already exists' }
+		const instance = new SimpleTokenInfo(props)
+		var res = await instance.save()
+		return { err: 0, msg: 'success', result: res }
+	} catch (ex) {
+		return { err: 1, msg: ex }
+	}
+}
+
+createStandardToken = async (props) => {
+	try {
+		var rows = await StandardToken.find({ tokenAddress: props?.tokenAddress, state: 10 })
+		if (rows.length > 0) return { error: 2, msg: 'already exists' }
+		const instance = new StandardTokenInfo(props)
+		var res = await instance.save()
+		return {err: 0, msg: 'success', result:res}
+	}catch (ex) {
+		return { err:1, msg: ex}
+	}
+}
+
+createReflectiontoken = async (props) => {
+	try {
+		var rows = await ReflectionToken.find({ tokenAddress: props?.tokenAddress, state: 10 })
+		if (rows.length > 0) return { error: 2, msg: 'already exists' }
+		const instance = new ReflectionTokenInfo(props)
+		var res = await instance.save()
+		return {err: 0, msg: "success", result:res}
+	} catch (ex) {
+		return { err:1, msg:ex}
+	}
+}
+
+createDevidedToken = async (props) => {
+	try {
+		var rows = await DevidendToken.find({ tokenAddress: props?.tokenAddress, state: 10 })
+		if (rows.length > 0) return { error: 2, msg: 'already exists' }
+		const instance = new DevidendTokenInfo(props)
+		var res = await instance.save()
+		return {err:0, msg:"success", result:res}
+	} catch (ex) {
+		return {err: 1, msg:ex}
+	}
+}
+
+createTokenLock = async(props) => {
+	try {
+		var rows = await TokenLock.find({ tokenAddress: props?.tokenAddress, state: 10 })
+		if (rows.length > 0) return { error: 2, msg: 'already exists' }
+		const instance = new TokenLockInfo(props)
+		var res = await instance.save()
+		return { err:0, msg:"success", result:res}
+	} catch (ex) {
+		return {err:1, msg:ex}
+	}
+}
+
+ApproveToken = async (address) => {
+	try{
+		const approve = await EasyModal.find({tokenAddress: address})
+		if(row.length > 0) return { error:1, result: true}
+		else return {error:0, msg: ex}
+	}catch (ex) {
+		return {err: 2, msg:ex}
+	}
+}
+
+setKYCVerified = async (id) => {
+	try {
+		var res = OwnerModal.updateOne({ _id: id }, { $set: { hasKYC: true } });
+		return { err: 0, result: true }
+	} catch (ex) {
+		return { err: 1, msg: ex }
+	}
+}
+
+existPool = async (address) => {
+	try {
+		var rows = await EasyModal.find({ tokenAddress: address })
+		if (rows.length > 0) return { error: 1, msg: 'already exists' }
+		else return { error: 0, msg: 'already exists' }
+	} catch (ex) {
+		return { err: 2, msg: ex }
+	}
+}
+
+createPool = async (props) => {
+	try {
+		var rows = await EasyModal.find({ tokenAddress: props?.tokenAddress, state: 10 })
+		if (rows.length > 0) return { error: 2, msg: 'already exists' }
+		const instance = new EasyModal(props)
+		var res = await instance.save()
+		const path = require('path')
+		var logopath1 = path.resolve(__dirname, "../public/temp/" + props?.token?.icon);
+		var imagepath1 = path.resolve(__dirname, "../public/temp/" + props?.token?.image);
+		var logopath2 = path.resolve(__dirname, "../public/logo/" + props?.token?.icon);
+		var imagepath2 = path.resolve(__dirname, "../public/img/" + props?.token?.image);
+		await fs.renameSync(logopath1, logopath2);
+		await fs.renameSync(imagepath1, imagepath2);
+		return { error: 0, msg: 'success', result: res }
+	} catch (ex) {
+		return { error: 1, msg: ex }
+	}
+}
+
+uploadImage = async (req, res) => {
+	try {
+		if (!req.file) {
+			return { err: 1 }
+		}
+		return { status: "success", id: filename };
+	} catch (ex) {
+		return { err: 1, msg: ex }
+	}
+}
+
+module.exports = {
+	getPoolsList,
+	getPoolInfo,
+	getMyPools,
+	ApproveToken,
+	createSimpleToken,
+	createStandardToken,
+	createReflectiontoken,
+	createDevidedToken,
+	createPool,
+	existPool,
+	setKYCVerified,
+	uploadImage
+}
